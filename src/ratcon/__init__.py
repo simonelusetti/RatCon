ab17f5c6f7df1270b844905d7b1d97696da49bc3
Simone Lusetti <simone.lusetti@unimore.it>
2025-11-20 10:07:48 +0100

    refactor into package

---
+++ src/ratcon/__init__.py
@@ -0,0 +1,13 @@
+"RatCon components packaged for reuse."
+
+from . import data, evaluate, losses, metrics, models, train, utils
+
+__all__ = [
+    "data",
+    "evaluate",
+    "losses",
+    "metrics",
+    "models",
+    "train",
+    "utils",
+]

43ab3250024d1ed1b443a1030dd336857ecb27bf
Simone Lusetti <simone.lusetti@unimore.it>
2025-12-04 12:21:08 +0100

    no more package

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"RatCon components packaged for reuse."
+
+from . import data, metrics, models, train, utils
+
+__all__ = [
+    "data",
+    "metrics",
+    "models",
+    "train",
+    "utils",
+]
